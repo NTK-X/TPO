2704e05934b57b59d6d7a0d194ea1629d7b06bfa
benjasantamarina <santamarinabenjamin@gmail.com>
2024-05-10 20:19:16 -0300

    Modificando el js3

--- Js/contacto.js
+++ Js/contacto.js
@@ -1,6 +1,29 @@
-var boton = document.getElementById("botonEnviar");
 
-boton.addEventListener("click", function(){
-    alert("Se hizo click en el boton");
-});
+const nombre = document.querySelector("[name=nombre]");
+const apellido = document.querySelector("[name=apellido]");
+const email = document.querySelector("[name=email]");
+const celular = document.querySelector("[name=celular]");
+const carrera = document.querySelector("[name=carrera]");
+const mensaje = document.querySelector("[name=mensaje]");
 
+nombre.addEventListener("focus", function(e){
+    const campo = e.target;
+    if(campo.value === "Error, este campo no puede estar vacio"){
+        campo.value = "";
+        campo.classList.remove("invalido");
+    }else{
+        campo.classList.remove("invalido");
+    }
+    
+
+})
+
+nombre.addEventListener("blur", function(e){
+    const campo = e.target;
+    const nombreValor = e.target.value;
+
+    if((nombreValor.length < 1) || (nombreValor === "Error, este campo no puede estar vacio")){
+        campo.classList.add("invalido");
+        e.target.value = "Error, este campo no puede estar vacio"
+    }
+})
